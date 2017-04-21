93fc6174ed825fa65db75d013a8a3dab7ef59842
geoclimber4 <georgedzuricsko1@gmail.com>
2017-04-21 13:41:10 -0500

    completed step 4 of 4.4

--- ruby/vampires.rb
+++ ruby/vampires.rb
@@ -1,47 +1,56 @@
-def boolean(boo)
-		if boo.to_s == "y"
-			boo = true
-		elsif boo.to_s == "n"
-			boo = false
-		else puts "Invald answer, please answer y or n"
+
+
+puts "How many employees will be processed?"
+num = gets.chomp.to_i
+until num == 0
+	
+	def boolean(boo)
+			if boo.to_s == "y"
+				boo = true
+			elsif boo.to_s == "n"
+				boo = false
+			else puts "Invald answer, please answer y or n"
+		end
 	end
-end
 
-puts "What is your name?"
-emp_name = gets.chomp
-puts "How old are you?"
-age = gets.chomp.to_i
-puts "What year were you born?"
-birth_year = gets.chomp.to_i
-def agecheck(age, birth_year)
-	if age == 2017 - birth_year
-		v_age = true
-	else
-		v_age = false
+	puts "What is your name?"
+	emp_name = gets.chomp
+	puts "How old are you?"
+	age = gets.chomp.to_i
+	puts "What year were you born?"
+	birth_year = gets.chomp.to_i
+	def agecheck(age, birth_year)
+		if age == 2017 - birth_year
+			v_age = true
+		else
+			v_age = false
+		end
 	end
+	agecheck = agecheck(age, birth_year)		
+	puts "Our company serves garlic bread. Should we order you some?"
+	garlic = gets.chomp
+	garlic = boolean(garlic)
+	puts "Would you like to enroll in the company's health insurance?"
+	insurance = gets.chomp
+	insurance = boolean(insurance)
+
+	puts emp_name
+	puts age
+	puts birth_year
+	puts garlic
+	puts insurance
+	puts agecheck
+
+
+	if agecheck == true && garlic == true
+		puts "Probably not a vampire"
+	elsif agecheck && garlic == false || insurance == false
+		puts "Probably a vampire"
+	elsif agecheck && garlic == false && inurance == false
+		puts "Almost certainly a vampire"
+	else puts "Results inconclusive"
+	end
+puts "Next employee please"
+num = num-1
 end
-agecheck = agecheck(age, birth_year)		
-puts "Our company serves garlic bread. Should we order you some?"
-garlic = gets.chomp
-garlic = boolean(garlic)
-puts "Would you like to enroll in the company's health insurance?"
-insurance = gets.chomp
-insurance = boolean(insurance)
-
-puts emp_name
-puts age
-puts birth_year
-puts garlic
-puts insurance
-puts agecheck
-
-
-
-if agecheck == true && garlic == true
-	puts "Probably not a vampire"
-elsif agecheck && garlic == false || insurance == false
-	puts "Probably a vampire"
-elsif agecheck && garlic == false && inurance == false
-	puts "Almost certainly a vampire"
-else puts "Results inconclusive"
-end
+puts "all done!"
