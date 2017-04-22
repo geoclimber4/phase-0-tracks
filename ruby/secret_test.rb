3697ba82469b69d1ba722a9c41b9b8734317e7c2
geoclimber4 <georgedzuricsko1@gmail.com>
2017-04-22 12:18:20 -0500

    added functional answer 4.6 release 4

--- ruby/secret_test.rb
+++ ruby/secret_test.rb
@@ -2,6 +2,22 @@
 #So "abc" would become "bcd". For now, you can assume lowercase input and output. 
 #Any space character should remain a space character -- no change made.
 
+#gets user input
+puts "Give me a string"
+string = gets.chomp
+
 def encrypt(string)
-	puts string.next
+	#indexes string for each letter (three letter max)
+	puts string[0].next + string[1].next + string[2].next
 end	
+
+#calls method
+puts encrypt(string)
+
+#A decrypt method that reverses the process above. 
+puts "Give me another string"
+string2 = gets.chomp
+
+def decrypt(string2)
+
+end
