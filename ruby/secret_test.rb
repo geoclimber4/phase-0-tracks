d5dd1bb4b17616ae87dc46c23571a24af5ebfa2d
geoclimber4 <georgedzuricsko1@gmail.com>
2017-04-22 12:03:41 -0500

    commit secret_test.rb

---
+++ ruby/secret_test.rb
@@ -0,0 +1,7 @@
+#An encrypt method that advances every letter of a string one letter forward. 
+#So "abc" would become "bcd". For now, you can assume lowercase input and output. 
+#Any space character should remain a space character -- no change made.
+
+def encrypt(string)
+	puts string.next
+end	
