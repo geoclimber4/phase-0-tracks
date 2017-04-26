21a9cc3e862b1c0349015705848ef56814a2b2e1
geoclimber4 <georgedzuricsko1@gmail.com>
2017-04-26 12:33:09 -0500

    completed release 1

---
+++ ruby/practice_hash.rb
@@ -0,0 +1,14 @@
+Job_application = {
+	"name" => "George",
+	"Address" => "123 Park ln",
+	"email" => "apple@gmail.com",
+	"phone" => "123-456-7890",
+	"shades_of_blue" => ["sky", "light", "powder", "navy", "dark"],
+	"Paisley" => [true],
+	"Chevrons" => [false],
+	"photorealistic woodsy scenes (with or without squirrels)" => [true],
+	"Abstract woodsy scenes (no squirresls)" => [false],
+	"Ombre" => ["Fierce", "So last season", "Practically medieval in its appalling irrelevance"],
+}
+
+p Job_application["name"]
