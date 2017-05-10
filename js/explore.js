10ea9b773013f3146bb264d0731ccb60f004fc20
geoclimber4 <georgedzuricsko1@gmail.com>
2017-05-10 18:32:12 -0500

    7.1

---
+++ js/explore.js
@@ -0,0 +1,6 @@
+// Javascript version of .reverse
+// Pseudocode: first loop the string for each letter using the counter code below
+// Then print each letter and add the strings together
+for (var i = 0; i < str.length; i++) {
+	console.log(str[i]);
+}
