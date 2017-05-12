2740af8e9209767646695466fe0c90f8f93808c1
geoclimber4 <georgedzuricsko1@gmail.com>
2017-05-11 19:37:38 -0500

    to fix overwrite

---
+++ js/data_structures.js
@@ -0,0 +1,48 @@
+// Declare 2 new arrays
+
+var colors = ["blue", "red", "fuchsia", "coral"];
+var names = ["Ed", "Seabiscuit", "American Pharoah", "Steve"];
+
+function add_color(color) {
+	colors.push(color);
+}
+
+function add_name(name) {
+	names.push(name);
+}
+
+var horses = {};
+
+for (var i = names.length - 1; i >= 0; i--) {
+	// horses.push {names[i]: colors[i];
+		horses[names[i]] = colors[i];
+}
+
+
+// var horses = {'names[0]': colors[0], 'Sea': colors[1]};
+// below code for testing only
+// var horses = {name: names[0], color: colors[0]}
+console.log(horses)
+
+// Driver Code
+add_color("black")
+add_name("Jack")
+
+console.log(colors)
+console.log(names)
+
+
+// Constructor function for car (release 2)
+
+
+function Car(make, model, color) {
+	this.make = make;
+	this.model = model;
+	this.color = color;
+	this.car_fast = function() { console.log("vrooooom!"); };
+};
+
+var newCar = new Car("Mazda", "Miata", "red");
+console.log(newCar);
+newCar.car_fast();
+
