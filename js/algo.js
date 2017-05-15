5d93570d2bd8db2f50d206965bef834e8a8695ce
geoclimber4 <georgedzuricsko1@gmail.com>
2017-05-15 11:33:04 -0500

    complete 7.3

--- js/algo.js
+++ js/algo.js
@@ -1,3 +1,5 @@
+// Release 0
+
 // First enter array of strings:
 var bikes = ["Motobecane Ti", "Cannondale Jekyll", "tricycle"];
 
@@ -13,8 +15,45 @@ function Longest_string(array){
    return array[0]
 }
  
+// Release 1
+// In algos.js, write a function that takes two objects and checks to see if the objects share at least one key-value pair. 
+
+function valueShare(objectOne, objectTwo) {
+// compare each key value pair. If they match result true
+  if(objectOne.name == objectTwo.name)
+    {return true;}
+  else {
+    return false;
+  }
+}
+
+// Release 2, random word array generator
+var array = [];
+function randomArray(num) {
+  limit = num; 
+  for (var i = 0; i < limit; i++) {
+    array.push(Math.round(Math.random()*10) * "c")
+  }
+  console.log(array)
+}
+
+// Driver Code
+// Release 0 Driver Code
 console.log(Longest_string(bikes));
 var cars = ["Honda", "Toyota Land Cruiser", "Subaru", "Fiat"];
 console.log(Longest_string(cars));
+
+// Release 1 Driver Code
+var person1 = {name: "Steven", age: 54};
+var person2 = {name: "Tamir", age: 54};
+
+var pet1 = {animal: "Dog", legs: 4};
+var pet2 = {animal: "Dog", legs: 3};
+console.log(valueShare(person1, person2));
+console.log(valueShare(pet1, pet2));
+
+
+// Driver code release 2
+console.log(randomArray(10))
 
 
